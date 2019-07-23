837cccdfbd5fd142f641a85202c8d7fba4b30a5a
soundarya-1609 <51950558+soundarya-1609@users.noreply.github.com>
2019-07-23 15:22:46 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,6 +1,6 @@
 ers1=int(input())
-ass1=list(map(int,input().split()[:ers1]))
-v=0
+as1=list(map(int,input().split()[:ers1]))
+vk=0
 for i in range(0,ers+1):
-   if(ass1.v(i)==1):
+   if(as1.vk(i)==1):
       print(i)
